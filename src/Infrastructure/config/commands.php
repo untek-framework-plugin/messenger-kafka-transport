972ee124a23cf3c23e91aeffbb055439d04143fa
Vitaliy <theyamshikov@yandex.ru>
2024-04-13 13:23:04 +0500

    upd

--- src/Infrastructure/config/commands.php
+++ src/Infrastructure/config/commands.php
@@ -3,6 +3,8 @@
 use Untek\FrameworkPlugin\MessengerKafkaTransport\Presentation\Cli\Commands\KafkaSendMessageCommand;
 use Untek\Framework\Console\Symfony4\Interfaces\CommandConfiguratorInterface;
 
+\Untek\Core\Code\Helpers\DeprecateHelper::hardThrow();
+
 return function (CommandConfiguratorInterface $commandConfigurator) {
     $commandConfigurator->registerCommandClass(KafkaSendMessageCommand::class);
 };
